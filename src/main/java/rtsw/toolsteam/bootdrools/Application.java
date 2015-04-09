aba2ada6828f09fd8a7efa980c54be4dfc5aa5d7
b1050502 <b1050502@A5470394.ne.nos.boeing.com>
2015-04-09 13:16:28 -0400

    Initial

--- src/main/java/rtsw/toolsteam/bootdrools/Application.java
+++ src/main/java/rtsw/toolsteam/bootdrools/Application.java
@@ -34,7 +34,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    
+  
     @Bean
     public KieContainer getKieContainer() throws FileNotFoundException{
         //return KieServices.Factory.get().getKieClasspathContainer();
